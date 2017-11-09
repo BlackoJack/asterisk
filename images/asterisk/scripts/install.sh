65f8201721a005c628c76837d1865ae35ecdb957
Раенко Иван <admin@sysadminsk.ru>
2017-11-09 09:06:26 +0700

    tmpfs cache rtp ports small bugfixes

--- images/asterisk/scripts/install.sh
+++ images/asterisk/scripts/install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "0" > /var/lib/asterisk/installed.txt
-
 mkdir -p /opt/sql
 
 apk add --update --no-cache expect tzdata lame \
@@ -14,5 +12,7 @@ mkdir /var/lib/asterisk/sounds/ru
 wget -P /var/lib/asterisk/sounds/ru/ http://downloads.asterisk.org/pub/telephony/sounds/asterisk-core-sounds-ru-gsm-current.tar.gz
 tar zxf /var/lib/asterisk/sounds/ru/asterisk-core-sounds-ru-gsm-current.tar.gz -C /var/lib/asterisk/sounds/ru/
 rm -f /var/lib/asterisk/sounds/ru/asterisk-core-sounds-ru-gsm-current.tar.gz
+
+echo "0" > /var/lib/asterisk/installed.txt
 
 asterisk && sleep 5
